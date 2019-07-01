9a780e4b1662a9fadada2d7d6a83b7693dad9eeb
GattalRaouf <gattal.raouf@gmail.com>
2019-07-01 21:40:54 +0100

    Updates

---
+++ precache-manifest.7ccd9487da08a556007664bc74a937cc.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e18e759e52228d05be3df5b07e073ddd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87e22f0c8b3b8b73c255",
+    "url": "/static/css/2.d497b198.chunk.css"
+  },
+  {
+    "revision": "c30f1f99acec35851453",
+    "url": "/static/css/main.3d91d1e7.chunk.css"
+  },
+  {
+    "revision": "87e22f0c8b3b8b73c255",
+    "url": "/static/js/2.c74d4b54.chunk.js"
+  },
+  {
+    "revision": "c30f1f99acec35851453",
+    "url": "/static/js/main.a814db12.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "608399dc715ed0049d93c0a5bf3b58df",
+    "url": "/static/media/GCE.608399dc.svg"
+  },
+  {
+    "revision": "1d68ca586c2ec70ea8c34f7a9f3286bb",
+    "url": "/static/media/GCE1.1d68ca58.svg"
+  },
+  {
+    "revision": "b0a22a04bd1857036ab70c7c0dc400a4",
+    "url": "/static/media/GDGCOOrganizer.b0a22a04.svg"
+  },
+  {
+    "revision": "1acf12649c0fc70b4441430701fc1587",
+    "url": "/static/media/Git.1acf1264.svg"
+  },
+  {
+    "revision": "1c525e7ab52f5d69ba98c6242454c97e",
+    "url": "/static/media/Graphic-Design.1c525e7a.svg"
+  },
+  {
+    "revision": "8670d30bb8fe5afcbbb63f845a6582de",
+    "url": "/static/media/Illustrator.8670d30b.svg"
+  },
+  {
+    "revision": "05a3e30fdf60ebe9d00cf4725d44bd42",
+    "url": "/static/media/React.05a3e30f.svg"
+  },
+  {
+    "revision": "63bc0c0bf5c1520018a80692d77d9984",
+    "url": "/static/media/SIGA_Project.63bc0c0b.png"
+  },
+  {
+    "revision": "c8b8558111b3ff57b2203be634596b20",
+    "url": "/static/media/XD.c8b85581.svg"
+  },
+  {
+    "revision": "bb9a18ec2c445cbb03e4a4ac536f1757",
+    "url": "/static/media/android.bb9a18ec.svg"
+  },
+  {
+    "revision": "2a524e9debb43d42252bd9f13efca94d",
+    "url": "/static/media/background1.2a524e9d.svg"
+  },
+  {
+    "revision": "b964573fb7ac7ae9278d180ac45f1e37",
+    "url": "/static/media/background2.b964573f.svg"
+  },
+  {
+    "revision": "be8243f095c7fa75e81c62a10d40663c",
+    "url": "/static/media/behance.be8243f0.svg"
+  },
+  {
+    "revision": "70ffe1c0841b5ac5e39f6581f21e13b1",
+    "url": "/static/media/database.70ffe1c0.svg"
+  },
+  {
+    "revision": "932d33b076f9b44ea599a42b129def73",
+    "url": "/static/media/github.932d33b0.svg"
+  },
+  {
+    "revision": "74624afc5e5fb6906327dd9113ce03c9",
+    "url": "/static/media/intern1.74624afc.svg"
+  },
+  {
+    "revision": "330502f05b89aef3bc77fc894cbe2e24",
+    "url": "/static/media/intern2.330502f0.svg"
+  },
+  {
+    "revision": "a732a6e5bc69653a55d6dd81ab8d2d5e",
+    "url": "/static/media/java.a732a6e5.svg"
+  },
+  {
+    "revision": "99fbbf55b7bfe0d3be16ed5663b0f9eb",
+    "url": "/static/media/kotlin.99fbbf55.svg"
+  },
+  {
+    "revision": "99c50a4722b2120feb52d69787499ba7",
+    "url": "/static/media/linkedin.99c50a47.svg"
+  },
+  {
+    "revision": "22c541f8c65e4f1502bd5fcc750ffaa1",
+    "url": "/static/media/me.22c541f8.svg"
+  },
+  {
+    "revision": "3d63000a52be99cab07f41de4d359101",
+    "url": "/static/media/medium.3d63000a.svg"
+  },
+  {
+    "revision": "5a4107b7e27ae447f702ded19b2d2f05",
+    "url": "/static/media/personalWebsite.5a4107b7.png"
+  },
+  {
+    "revision": "332b34cc7bc61a17bdfb54fb14719962",
+    "url": "/static/media/portfolio.332b34cc.svg"
+  },
+  {
+    "revision": "60218c327499055e3301edd6c1817209",
+    "url": "/static/media/ship.60218c32.svg"
+  },
+  {
+    "revision": "90f812f628fb5786c96980d59de71c37",
+    "url": "/static/media/study-icon.90f812f6.svg"
+  },
+  {
+    "revision": "ded62512c4df7ee92226e746115e111f",
+    "url": "/static/media/talk.ded62512.svg"
+  },
+  {
+    "revision": "c033f77a51daf5912e626c276c5aaa8d",
+    "url": "/static/media/twitter.c033f77a.svg"
+  },
+  {
+    "revision": "a11cd7710e10569dcdbd2f3d84afa4f0",
+    "url": "/static/media/undergraduate.a11cd771.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/abderraouf-resume/precache-manifest.80394704691b02e7c415fa4a109c3e6b.js"
+  "/precache-manifest.7ccd9487da08a556007664bc74a937cc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/abderraouf-resume/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
